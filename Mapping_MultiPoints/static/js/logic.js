6af2a6542b9d47a6e8dd442f4f5ca091db6c6b0a
robbe-verhofste <robbe.verhofste@tkdesignlab.com>
2021-02-28 17:44:39 -0600

    aiports

--- Mapping_MultiPoints/static/js/logic.js
+++ Mapping_MultiPoints/static/js/logic.js
@@ -1,9 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center and zoom level.
+let map = L.map('mapid').setView([30, 30], 2);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{style}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -17,6 +16,17 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{style}/tiles
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+
+// Accessing the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+
+
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data).addTo(map);
+});
 
 // An array containing each city's location, state, and population.
 let cities = [{
