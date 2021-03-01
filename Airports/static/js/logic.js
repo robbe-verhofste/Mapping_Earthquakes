7fa6714141987d4ca15bbddc0391fcb0242a9d35
robbe-verhofste <robbe.verhofste@tkdesignlab.com>
2021-02-28 18:08:34 -0600

    airports

--- Airports/static/js/logic.js
+++ Airports/static/js/logic.js
@@ -18,7 +18,7 @@ streets.addTo(map);
 
 
 // Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/robbe-verhofste/Mapping_Earthquakes/Major_Airports/Airports/majorAirports.json";
 
 
 // Grabbing our GeoJSON data.
@@ -28,75 +28,89 @@ d3.json(airportData).then(function(data) {
   L.geoJson(data).addTo(map);
 });
 
-// An array containing each city's location, state, and population.
-let cities = [{
-    location: [40.7128, -74.0059],
-    city: "New York City",
-    state: "NY",
-    population: 8398748
-  },
-  {
-    location: [41.8781, -87.6298],
-    city: "Chicago",
-    state: "IL",
-    population: 2705994
-  },
-  {
-    location: [29.7604, -95.3698],
-    city: "Houston",
-    state: "TX",
-    population: 2325502
-  },
-  {
-    location: [34.0522, -118.2437],
-    city: "Los Angeles",
-    state: "CA",
-    population: 3990456
-  },
-  {
-    location: [33.4484, -112.0740],
-    city: "Phoenix",
-    state: "AZ",
-    population: 1660272
-  }
-  ];
-
-
-    // Loop through the cities array and create one marker for each city.
-    cities.forEach(function(city) {
-        console.log(city)
-        L.marker(city.location).addTo(map);
-    });
-
-
-    // Loop through the cities array and create one marker for each city.
-    cities.forEach(function(city) {
-        console.log(city)
-        L.circleMarker(city.location, {
-            "radius": city.population/100000,
-        }).addTo(map);
-        L.marker(city.location)
-            .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-                .addTo(map);
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data)
+            .bindPopup("<h2>" + properties.city)
         // let line = [[city.location]];
         // L.polyline(line, {
         //     color: "red"
         // }).addTo(map);
-    });
+    .addTo(map);
+});
 
 
-    let line = [
-        [33.9416, -118.4085],
-        [37.6213, -122.3790]
-      ];
+// // An array containing each city's location, state, and population.
+// let cities = [{
+//     location: [40.7128, -74.0059],
+//     city: "New York City",
+//     state: "NY",
+//     population: 8398748
+//   },
+//   {
+//     location: [41.8781, -87.6298],
+//     city: "Chicago",
+//     state: "IL",
+//     population: 2705994
+//   },
+//   {
+//     location: [29.7604, -95.3698],
+//     city: "Houston",
+//     state: "TX",
+//     population: 2325502
+//   },
+//   {
+//     location: [34.0522, -118.2437],
+//     city: "Los Angeles",
+//     state: "CA",
+//     population: 3990456
+//   },
+//   {
+//     location: [33.4484, -112.0740],
+//     city: "Phoenix",
+//     state: "AZ",
+//     population: 1660272
+//   }
+//   ];
+
+
+//     // Loop through the cities array and create one marker for each city.
+//     cities.forEach(function(city) {
+//         console.log(city)
+//         L.marker(city.location).addTo(map);
+//     });
+
+
+//     // Loop through the cities array and create one marker for each city.
+//     cities.forEach(function(city) {
+//         console.log(city)
+//         L.circleMarker(city.location, {
+//             "radius": city.population/100000,
+//         }).addTo(map);
+//         L.marker(city.location)
+//             .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//                 .addTo(map);
+//         // let line = [[city.location]];
+//         // L.polyline(line, {
+//         //     color: "red"
+//         // }).addTo(map);
+//     });
+
+
+//     let line = [
+//         [33.9416, -118.4085],
+//         [37.6213, -122.3790]
+//       ];
     
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "red"
-  }).addTo(map);
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//     color: "red"
+//   }).addTo(map);
   
-// TOOLS
-// https://docs.mapbox.com/help/glossary/
+// // TOOLS
+// // https://docs.mapbox.com/help/glossary/
 
 // Map styles:
 // mapbox.streets
